2d89f9e9e31f61d766cf5f12db678268cae99756
20wang <shanw2020@gmail.com>
2020-04-14 15:12:49 -0400

    what's up, nerds

--- board.py
+++ board.py
@@ -1,12 +1,24 @@
 import numpy as np
-from random import randint
+from random import randrange
 
 
 # places mines on a numpy array
-def setmines(w, h, n):
-    mines = np.zeros((h, w))
-    for i in range(n):
-        tempspace = (randint(0, h), randint(0, w))
+def set_mines(w, h, n):
+    mine_locations = np.zeros((h, w))
+    mines = []
+
+    i = 0
+    while i < n:
+        temp_space = (randrange(0, h), randrange(0, w))
+        if temp_space not in mines:
+            mines.append(temp_space)
+            i += 1
+
+    for mine in mines:
+        mine_locations[mine[0], mine[1]] = 1
+
+    return mine_locations
+
 
 if __name__ == '__main__':
     # information about board, depends on dificulty
@@ -14,5 +26,7 @@ if __name__ == '__main__':
     height = 16
     number = 40
 
-    mines = setmines(width, height, number)
+    mine_map = set_mines(width, height, number)
     board = np.zeros((height, width))
+
+    
