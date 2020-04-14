921dd43395571e1cc28ba158fcea355efbef3cff
20wang <shanw2020@gmail.com>
2020-04-14 14:58:43 -0400

    what's up, nerds

---
+++ board.py
@@ -0,0 +1,18 @@
+import numpy as np
+from random import randint
+
+
+# places mines on a numpy array
+def setmines(w, h, n):
+    mines = np.zeros((h, w))
+    for i in range(n):
+        tempspace
+
+if __name__ == '__main__':
+    # information about board, depends on dificulty
+    width = 16
+    height = 16
+    number = 40
+
+    mines = setMines(width, height, number)
+    board = np.zeros((height, width))
