abbeffa791ecebf8775024153bd17f6229c95873
20wang <shanw2020@gmail.com>
2020-04-16 15:23:24 -0400

    what's up, nerds

--- board.py
+++ board.py
@@ -29,23 +29,34 @@ class Board:
     # click = 0 for left click, 1 for right click
     # returns true if the game is lost, false if not
     def make_move(self, x, y, click):
+
         if click == 1:
-            self.play_board[x, y] = -1
+            self.play_board[y, x] = -1
 
         elif click == 0:
-
             # checks if game is lost
-            if self.mine_map[x, y] == 1.0:
-                self.play_board[x, y] = -3
+            if self.mine_map[y, x] == 1.0:
+                self.play_board[y, x] = -3
                 return True
 
             # checks adjacent squares for mines
             m = 0
+            check_list = []
             for adj_x in range(-1, 2):
                 for adj_y in range(-1, 2):
-                    if self.mine_map[adj_x, adj_y] == 1.0:
-                        m += 1
-            self.play_board[x, y] = m
+                    if 0 <= x + adj_x < self.width and 0 <= y + adj_y < self.height and not\
+                            (adj_x == 0 and adj_y == 0):
+                        check_list.append((y + adj_y, x + adj_x))
+            for c in check_list:
+                if self.mine_map[c[0], c[1]] == 1.0:
+                    m += 1
+            self.play_board[y, x] = m
+
+            # auto-moves on zeroes if no adjacent mines, like real minesweeper does
+            if m == 0:
+                for c in check_list:
+                    if self.play_board[c[0], c[1]] == -2:
+                        self.make_move(c[1], c[0], 0)
 
         return False
 
@@ -55,7 +66,3 @@ class Board:
     # 0+: number of mines adjacent to explored space
     def get_board(self):
         return self.play_board
-
-
-
-
