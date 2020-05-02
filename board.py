unflagging is playable, wrong flags -.7 reward, loss averaged properly, board prints max 5 turns

board.py
@@ -49,7 +49,7 @@ class Board:
                         win = False
             if flags == self.number and win:
                 return True, 1
-            return False, 0      # ??????????? bruh what reward?
+            return False, -.7      # ??????????? bruh what reward?
 
         elif click == 0:
             if self.blank and self.mine_map[y, x] == 1:
@@ -128,7 +128,5 @@ class Board:
         b = self.get_board()
         net_board = np.empty((2,) + b.shape, dtype=np.float32)
         net_board[0, :, :] = np.maximum(b, 0)
-        net_board[1, :, :] = (b == -2)
-        print(b)
-        print(net_board)
+        net_board[1, :, :] = (b == -2) | (b == -1)
         return net_board

sweepNet.py
@@ -49,5 +49,5 @@ class PolicyLoss(nn.Module):
     def forward(self, policy, action, advantage):
         # advantage ~= reward - baseline
         policy_slice = policy.gather(1, action.view(-1,1))
-        loss = - torch.sum(torch.log(policy_slice) * advantage)
+        loss = - (torch.log(policy_slice) * advantage).mean()
         return loss